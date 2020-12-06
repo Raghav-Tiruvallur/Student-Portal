333862d3461d9168222bbe5c807ed3d89a30900f
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-06 18:56:01 +0530

    models added

---
+++ user.js
@@ -0,0 +1,19 @@
+const express=require('express');
+
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true
+    }
+})
+
+
+module.exports=mongoose.model('Users',userSchema);
