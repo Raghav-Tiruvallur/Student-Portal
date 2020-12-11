b752653551e689b01797a43fbe96836156480946
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:32:46 +0530

    Create RightLink.js

---
+++ src/Components/RightLink.js
@@ -0,0 +1,18 @@
+import React from 'react';
+class RightLink extends React.Component
+{
+	render()
+	{
+		return(
+	<div id="rightAnchor">
+		<ul>
+		<li><a href="www.google.com">About University</a></li>
+		<li><a href="www.google.com">Contact</a></li>
+		<li><a href="www.google.com">Admission</a></li>
+		<li><a href="www.google.com">Academics</a></li>
+		<li><a href="www.google.com">Clubs</a></li>
+		</ul>
+		</div>)
+	}
+} 
+export default RightLink;
