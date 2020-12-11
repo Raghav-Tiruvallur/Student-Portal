619c8725f4ec07fbf5b765781040694394961a7a
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:26:05 +0530

    Update EventRegister.js

--- src/Components/EventRegister.js
+++ src/Components/EventRegister.js
@@ -1,56 +1,70 @@
 import React,{useState} from 'react';
 import axios from 'axios';
-import {useHistory} from 'react-router-dom';
-function EventRegister()
+import {createBrowserHistory} from 'history';
+import Swal from 'sweetalert2';
+class EventRegister extends React.Component
 {
-    const styles={
-        textAlign:"center",
-        color:"green",
-        fontSize:30,
-        fontFamily:"Lucida Console"
-    }
-    const [email,setemail]=useState('');
-    const [club,setclub]=useState('');
-    const [event,setevent]=useState('');
-    const history=useHistory();
-    function RegisterfunctionEvent(e)
+	constructor(props)
+	{
+		super(props)
+		this.RegisterfunctionEvent=this.RegisterfunctionEvent.bind(this)
+		this.render=this.render.bind(this)
+		this.email=React.createRef()
+		this.ename=React.createRef()
+		this.club=React.createRef()
+	}
+	RegisterfunctionEvent(e)
+	{
+		e.preventDefault()
+		let d={
+			email:this.email.current.value,
+			Event_Name:this.ename.current.value,
+			Club:this.club.current.value
+		}
+		axios.post("http://localhost:5000/event/EventRegister",d)
+			.then((resp)=>{
+				if(resp){
+					Swal.fire({
+						position:"top-right",
+						icon:'success',
+						title:'Successfully Registered'
+					});
+				}              
+				else 
+				{
+					Swal.fire({
+						position:"top-right",
+						icon:'error',
+						title:'Something went wrong'
+					})
+				}
+			})
+			.catch((err)=>console.log(err));
+	}
+    render()
     {
-        e.preventDefault();
-        let d={
-            email:email,
-            Event_Name:event,
-            Club:club
-        }
-        axios.post("http://localhost:5000/event/EventRegister",d)
-        .then((resp)=>{
-            if(resp){
-                alert("Succesful Register");
-                history.push('/event');
-            }              
-            else 
-                alert("Something went wrong");
-        })
-        .catch((err)=>console.log(err));
-    }
-    return(
-        <div className="container mt-5">
-    <form  onSubmit={(e)=>RegisterfunctionEvent(e)}  style={{marginLeft:"20%",marginRight:"20%",marginTop:"5%"}} className="col-md-5 mx-auto">
-    <div class="form-group">
-    <h1 style={{WebkitTextFillColor:"white"}}>Registration Confirmation</h1><br/>
-    <label style={{WebkitTextFillColor:"white"}} for="exampleInputEmail1">Student Email</label>
-    <input type="email" value={email} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Student Email" onChange={(e)=>setemail(e.target.value)} required/>
-  </div>
-  <div class="form-group">
-    <label style={{WebkitTextFillColor:"white"}} for="exampleInputPassword1">Event Name</label>
-    <input type="text" value={event} className="form-control"placeholder="Event Name" onChange={(e)=>setevent(e.target.value)} required/>
-  </div>
-  <div class="form-group">
-    <label style={{WebkitTextFillColor:"white"}} for="exampleInputPassword1">Club</label>
-    <input type="text" value={club} className="form-control"placeholder="Club" onChange={(e)=>setclub(e.target.value)} required/>
-  </div>
-  <button type="submit"className="btn btn-primary btn-sm m-10">Confirm</button><br/><br/>
-</form>
+    	return(
+       		<div id="regboxE">
+			<form  onSubmit={(e)=>this.RegisterfunctionEvent(e)}  style={{marginLeft: "50px",
+				marginRight: "50px",
+				marginTop: "5%",
+				position: "relative",
+				width: "400px",
+				padding: 0}} className="col-md-5 mx-auto">
+			<div className="form-group">
+			<label style={{WebkitTextFillColor:"white"}} for="email">Student E-mail</label>
+			<input type="email" className="form-control" id="email" placeholder="E-mail:" aria-describedby="emailHelp" ref={this.email} autoComplete="on" required/>
+			</div>
+			<div className="form-group">
+			<label style={{WebkitTextFillColor:"white"}} for="club">Club</label>
+			<input type="text" className="form-control" id="club"ref={this.club} value={this.props.club} readonly required/><br/>
+			<label style={{WebkitTextFillColor:"white"}} for="ename">Event Name</label>
+			<input type="text" className="form-control" id="ename" ref={this.ename} value={this.props.event} readonly required/><br/>
+			<button type="submit" id="regbtn"className="btn btn-primary btn-sm m-10">Register</button>
+			</div>
+			</form>
 </div>
 )
+}
 }
 export default EventRegister;
