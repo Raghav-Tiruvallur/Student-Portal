c2752e82847a797d732f0423a6d5d213152ab50c
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:31:36 +0530

    Update Register.js

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,61 +1,104 @@
-import React,{useState} from 'react';
+import React from 'react';
+import ReactDOM from "react-dom";
 import Swal from 'sweetalert2';
-import {Link,useHistory} from 'react-router-dom';
+import "../Design.css"
+import RightLink from "./RightLink.js"
 const axios=require('axios');
-function Register()
+
+class Register extends React.Component
 {
-    const [email,setemail]=useState('');
-    const [pass,setpass]=useState('');
-    const history=useHistory();
-    function Registerfunction(e)
-    {
-        e.preventDefault();
-        let d={
-            email:email,
-            password:pass
-        }
-        axios.post("http://localhost:5000/register",d)
-        .then((resp)=>{
-             if(resp){
-                Swal.fire({
-                    position:"top-right",
-                    icon:'success',
-                    title:'Successfully Registered'
-                  })
-                history.push('/');
-            }              
-            else 
-            {
-                Swal.fire({
-                    position:"top-right",
-                    icon:'error',
-                    title:'Something went wrong'
-                  })
-            }
-        })
-        .catch((err)=>console.log(err));
-    }
+	constructor(props)
+	{
+		super(props)
+		this.Registerfunction=this.Registerfunction.bind(this)
+		this.render=this.render.bind(this)
+		this.email=React.createRef()
+		this.pass=React.createRef()
+	}
+	Registerfunction(e)
+	{
+		e.preventDefault();
+		let d={
+			email:this.email.current.value,
+			password:this.pass.current.value
+		}
+		axios.post("http://localhost:5000/register",d)
+			.then((resp)=>{
+				if(resp){
+					Swal.fire({
+						position:"top-right",
+						icon:'success',
+						title:'Successfully Registered'
+					})
 
-    return(
+					this.backFunc()
+				}              
+				else 
+				{
+					Swal.fire({
+						position:"top-right",
+						icon:'error',
+						title:'Something went wrong'
+					})
+				}
+			})
+			.catch((err)=>console.log(err));
+	}
+	backFunc()
+	{
+		var dest=document.getElementById("rightHalf")
+		ReactDOM.render(<RightLink/>,dest)
+	}
+	confirmFunc()
+	{
+		var x=document.getElementById("Cpassr")
+		var y=document.getElementById("passr")
+		var btn = document.getElementById("regbtn")
+		var dest=document.getElementById("Cdest")
+		if(x.value!==y.value)
+		{
+				btn.disabled=true;
+				ReactDOM.render(<small><i>Passwords Do Not Match</i></small>,dest)
+		} else {
+			btn.disabled=false;
+			ReactDOM.render(<></>,dest)
+		}
+	}
+	render(){
+		return(
+			<>
+			<div id="regbox">
+			<form  onSubmit={(e)=>this.Registerfunction(e)}  style={{marginLeft: "50px",
+				marginRight: "50px",
+				marginTop: "5%",
+				position: "relative",
+				width: "400px",
+				padding: 0}} className="col-md-5 mx-auto">
+			<div className="form-group">
+			<h1 style={{WebkitTextFillColor:"white"}}>User Registration</h1><br/>
+			<label style={{WebkitTextFillColor:"white"}} for="emailr">Student E-mail</label>
+			<input type="email" className="form-control" id="emailr" placeholder="E-mail:" aria-describedby="emailHelp" ref={this.email} autoComplete="off" required/>
+			</div>
+			<div className="form-group">
+			<label style={{WebkitTextFillColor:"white"}} for="passr">Password</label>
+			<input type="password" className="form-control" id="passr" placeholder="Password:" ref={this.pass} autocomplete="off" required/><br/>
+			<label style={{WebkitTextFillColor:"white"}} for="Cpassr">Confirm Password</label>
+			<input type="password" className="form-control" id="Cpassr" placeholder="Confirm Password:" autoComplete="off" onChange={this.confirmFunc} required/><br/>
+			<button type="submit" id="regbtn"className="btn btn-primary btn-sm m-10">Register</button>
+			<span id='Cdest'></span>
+			<br/><br/>
+			</div>
+			</form>
+			</div>
+			<div id="backButton">
+			<ul>
+			<li><a onClick={this.backFunc}>Go Back</a></li>
+			</ul>
+			</div>
+			</>
 
-    <div className="container mt-5">
-    <form  onSubmit={(e)=>Registerfunction(e)}  style={{marginLeft:"20%",marginRight:"20%",marginTop:"5%"}} className="col-md-5 mx-auto">
-    <div class="form-group">
-    <h1 style={{WebkitTextFillColor:"white"}}>Welcome To Student Portal</h1><br/>
-    <h3 style={{WebkitTextFillColor:"white"}}>Register Here</h3><br/>
-    <label style={{WebkitTextFillColor:"white"}} for="exampleInputEmail1">Student Email</label>
-    <input type="email" value={email} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Student Email" onChange={(e)=>setemail(e.target.value)} required/>
-    <small id="emailHelp" className="form-text text-muted">Trust us.<br/>We'll never share your id with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label style={{WebkitTextFillColor:"white"}} for="exampleInputPassword1">Password</label>
-    <input type="password" value={pass} className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={(e)=>setpass(e.target.value)} required/>
-  </div>
-  <button type="submit"className="btn btn-primary btn-sm m-10">Register</button><br/><br/>
-</form>
-<Link to='/'  className="text-centre mt-5"><h5>Login Here</h5></Link>
-</div>
-);
+		)
+	}
 }
- 
+
 export default Register;
