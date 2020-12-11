3c45826f5ecd66afcb1794ad61798bf3fb38c704
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:28:28 +0530

    Update HollywoodQuiz.js

--- src/Components/HollywoodQuiz.js
+++ src/Components/HollywoodQuiz.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
+import EventRegister from "./EventRegister.js"
+import "../EventStyle.css"
 function Hollywood(){
-    return(
-        <>
-        <div>
-        <h1 style={{color:"green",textAlign:"center"}}>Hollywood Quiz</h1><br/>
-        <p style={{color:'blue',textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Hollywood is one of the biggest industries in the world.  Everyone loves the movies.
+	return(
+		<div class="clubpage">
+		<div class="topHalf">
+		<h1 style={{color:"#4caf50",textAlign:"center"}}>Hollywood Quiz</h1><br/>
+		<p style={{WebkitTextFillColor:"white",textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Hollywood is one of the biggest industries in the world.  Everyone loves the movies.
         <br/>Now it's time to put your skills to the test.<br/>  We introduce the Hollywood Quiz where we test you on your Hollywood knowledge.<br/>
         We hope to see you there!!<br/>
         Time: 10:00 am<br/>
         Date: 15/11/2020<br/>
         Amount:Rs.200<br/>
-        #Payment to be made on the day 
-        </p>
-        </div>
-       <Link to='/event/EventRegister'><button style={{marginLeft:'710px',marginTop:'100px',fontSize:20}} className="btn btn-primary btn-sm m-10">Register</button></Link>
-        </>
-    )
+		</p>
+		<button style={{fontSize: "20px",
+			position: "absolute",
+			top: "25vh",
+			right: "10vw"}} className="btn btn-primary btn-sm m-10" onClick={function(){ReactDOM.render(
+				<EventRegister club="Quiz Club" event="Hollywood Quiz"/>,document.getElementById("bottomAnchor")
+			)}}>Register</button>
+		</div>
+		<div class="bottomHalf">
+		<div id="bottomAnchor"> </div>
+		</div>
+		</div>
+
+
+	)
 }
 export default Hollywood;
