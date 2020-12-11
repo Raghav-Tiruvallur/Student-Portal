acb8c62b2fdb05b2470fdb27ba2dbb1fd8435c05
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:23:53 +0530

    Update CricketQuiz.js

--- src/Components/CricketQuiz.js
+++ src/Components/CricketQuiz.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
- 
-function Cricket(){
-    return(
-        <>
-        <div>
-        <h1 style={{color:"green",textAlign:"center"}}>Cricket Quiz</h1><br/>
-        <p style={{color:'blue',textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Love IPL?? What about WCs?
+import ReactDOM from 'react-dom';
+import EventRegister from "./EventRegister.js"
+import "../EventStyle.css"
+function CricketQuiz(){
+	return(
+		<div class="clubpage">
+		<div class="topHalf">
+		<h1 style={{color:"#4caf50",textAlign:"center"}}>Cricket Quiz</h1><br/>
+		<p style={{WebkitTextFillColor:"white",textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Love IPL?? What about WCs?
         <br/>Let's test your knowledge<br/>  We introduce the Cricket Quiz!! <br/>
         We hope to see you there!!<br/>
         Time: 12:00 noon<br/>
         Date: 15/11/2020<br/>
         Amount:Rs.200<br/>
-        #Payment to be made on the day 
-        </p>
-        </div>
-       <Link to='/event/EventRegister'><button style={{marginLeft:'710px',marginTop:'100px',fontSize:20}} className="btn btn-primary btn-sm m-10">Register</button></Link>
-        </>
-    )
+		</p>
+		<button style={{fontSize: "20px",
+			position: "absolute",
+			top: "25vh",
+			right: "10vw"}} className="btn btn-primary btn-sm m-10" onClick={function(){ReactDOM.render(
+				<EventRegister club="Quiz Club" event="Cricket Quiz"/>,document.getElementById("bottomAnchor")
+			)}}>Register</button>
+		</div>
+		<div class="bottomHalf">
+		<div id="bottomAnchor"> </div>
+		</div>
+		</div>
+
+
+	)
 }
-export default Cricket;
+export default CricketQuiz;
