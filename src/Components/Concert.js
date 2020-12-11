3d61fb76ca85ca14548795d0322ca2dfe30b4ea1
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:22:39 +0530

    Update Concert.js

--- src/Components/Concert.js
+++ src/Components/Concert.js
@@ -1,23 +1,32 @@
 import React from 'react';
-
-import {Link} from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
+import EventRegister from "./EventRegister.js"
+import "../EventStyle.css"
 function Concert(){
-    return(
-        <>
-        <div>
-        <h1 style={{color:"green",textAlign:"center"}}>Concert Night</h1><br/>
-        <p style={{color:'blue',textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>This is the night you've all been waiting for!!
+	return(
+		<div class="clubpage">
+		<div class="topHalf">
+		<h1 style={{color:"#4caf50",textAlign:"center"}}>Karaoke Night</h1><br/>
+		<p style={{WebkitTextFillColor:"white",textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>This is the night you've all been waiting for!!
         <br/>The stage is going to be on fire!!<br/>You're all going to have an amazing time!!!<br/>
         We hope to see you there!!<br/>
         Time: 6:00 pm<br/>
         Date: 21/11/2020<br/>
         Amount:Rs.500 per head<br/>
-        #Payment to be made on the day 
-        </p>
-        </div>
-       <Link to='/event/EventRegister'><button style={{marginLeft:'710px',marginTop:'100px',fontSize:20}} className="btn btn-primary btn-sm m-10">Register</button></Link>
-        </>
-    )
+		</p>
+		<button style={{fontSize: "20px",
+			position: "absolute",
+			top: "25vh",
+			right: "10vw"}} className="btn btn-primary btn-sm m-10" onClick={function(){ReactDOM.render(
+				<EventRegister club="Music Club" event="Concert Night"/>,document.getElementById("bottomAnchor")
+			)}}>Register</button>
+		</div>
+		<div class="bottomHalf">
+		<div id="bottomAnchor"> </div>
+		</div>
+		</div>
+
+
+	)
 }
-export default Concert;
+export default Concert
