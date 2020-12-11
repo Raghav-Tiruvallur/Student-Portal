68bab718a9d1e568a6a3bb724e8ab4f109886033
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:18:57 +0530

    Update Basketball.js

--- src/Components/Basketball.js
+++ src/Components/Basketball.js
@@ -1,23 +1,32 @@
 import React from 'react';
-
-import {Link} from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
+import EventRegister from "./EventRegister.js"
+import "../EventStyle.css"
 function Basketball(){
-    return(
-        <>
-        <div>
-        <h1 style={{color:"green",textAlign:"center"}}>Basketball</h1><br/>
-        <p style={{color:'blue',textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Tired of seeing NBA on TV?  Want to actually play some basketball?
-        <br/>We are there for you,let's get the match started<br/>You excited? We sure are!!<br/>
-        We hope to see you there!!<br/>
-        Time: 10:00 am<br/>
-        Date: 18/11/2020<br/>
-        Amount:Rs.300<br/>
-        #Payment to be made on the day 
-        </p>
-        </div>
-       <Link to='/event/EventRegister'><button style={{marginLeft:'710px',marginTop:'100px',fontSize:20}} className="btn btn-primary btn-sm m-10">Register</button></Link>
-        </>
-    )
+	return(
+		<div class="clubpage">
+		<div class="topHalf">
+		<h1 style={{color:"#4caf50",textAlign:"center"}}>Basketball Tournament</h1><br/>
+		<p style={{WebkitTextFillColor:"white",textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Tired of seeing NBA on TV?  Want to actually play some basketball?
+		<br/>We are there for you,let's get the match started<br/>You excited? We sure are!!<br/>
+		We hope to see you there!!<br/>
+		Time: 10:00 am<br/>
+		Date: 18/12/2020<br/>
+		Entry Fee:Rs.300<br/> 
+		</p>
+		<button style={{fontSize: "20px",
+			position: "absolute",
+			top: "25vh",
+			right: "10vw"}} className="btn btn-primary btn-sm m-10" onClick={function(){ReactDOM.render(
+				<EventRegister club="Sports Club" event="Basketball"/>,document.getElementById("bottomAnchor")
+			)}}>Register</button>
+		</div>
+		<div class="bottomHalf">
+		<div id="bottomAnchor"> </div>
+		</div>
+		</div>
+
+
+	)
 }
 export default Basketball;
