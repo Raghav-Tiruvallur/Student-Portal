c175009b7979c0b589c3cbebcb71edfd2225c2d0
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:20:01 +0530

    Update CardDes.jsx

--- src/Components/CardDes.jsx
+++ src/Components/CardDes.jsx
@@ -4,51 +4,129 @@ import '../Card.css';
 import {Link} from 'react-router-dom';
 
 class CardHeader extends React.Component {
-    render() {
-        const { image } = this.props;
-        var style = { 
-            backgroundImage: 'url(' + image + ')',
-        };
-        return (
-            <header style={style} id={image} className="card-header">
-            </header>
-        )
-    }
+	render() {
+		const { image } = this.props;
+		var style = { 
+			backgroundImage: 'url(' + image + ')',
+		};
+		return (
+			<header style={style} id={image} className="card-header">
+			</header>
+		)
+	}
 }
-  
-class Button extends React.Component {
-render() {
-    var margin={
-        marginLeft:"80px",
-        width:"75px",
-        textAlign:"center"
-    }
-    return (
-      <button className="btn btn-primary btn-sm m-10" style={margin}>View</button>
-    )
-}
+class CardBody extends React.Component {
+	render() {
+		return (
+			<div className="card-body">
+			<p className="body-content">{this.props.title}</p>
+			</div>
+		)
+	}
 }
 
-class CardBody extends React.Component {
-render() {
-    return (
-    <div className="card-body">
-        <h2>{this.props.title}</h2>
-        <p className="body-content">{this.props.text}</p>
-        <Link to={this.props.linkto}><Button /></Link>
-    </div>
-    )
+class Card1 extends React.Component
+{
+
+	func=(e)=>
+	{
+		var divdest=document.getElementsByClassName("cardWrapper1")[0].firstChild
+		var dest=document.getElementsByClassName("cardWrapper1")[0].firstChild.firstChild.lastChild
+		if(e.type==="mouseover"){
+			dest.style.color="#4caf50"
+			divdest.style.boxShadow="2px 2px 2px 2px #fff"}
+		else if(e.type==="mouseleave"){
+			dest.style.color="white"
+			divdest.style.boxShadow="0px 0px 0px 0px"}
+	}
+	render()
+	{
+		return(
+			<div className="cardWrapper1" >	
+			<article className="card"onMouseOver={this.func} onMouseLeave={this.func}>
+			<Link to={this.props.prop.linkto}>
+			<CardHeader image={this.props.prop.image} link={this.props.prop.linkto}/>
+			<CardBody title={this.props.prop.title}/>
+			</Link>
+			</article>
+			</div>
+		)
+	}
 }
+
+class Card2 extends React.Component
+{
+	func=(e)=>
+	{
+		var divdest=document.getElementsByClassName("cardWrapper2")[0].firstChild
+		var dest=document.getElementsByClassName("cardWrapper2")[0].firstChild.firstChild.lastChild
+		if(e.type==="mouseover"){
+			dest.style.color="#4caf50"
+			divdest.style.boxShadow="2px 2px 2px 2px #fff"}
+		else if(e.type==="mouseleave"){
+			dest.style.color="white"
+			divdest.style.boxShadow="0px 0px 0px 0px"}
+	}
+	render()
+	{
+		return(
+			<div className="cardWrapper2" >
+			<article className="card" onMouseOver={this.func} onMouseLeave={this.func}>
+			<Link to={this.props.prop.linkto}>	
+			<CardHeader image={this.props.prop.image} link={this.props.prop.linkto}/>
+			<CardBody title={this.props.prop.title}/>
+			</Link>
+			</article>
+			</div>
+		)
+	}
+}
+
+class Card3 extends React.Component
+{
+	func=(e)=>
+	{
+		var divdest=document.getElementsByClassName("cardWrapper3")[0].firstChild
+		var dest=document.getElementsByClassName("cardWrapper3")[0].firstChild.firstChild.lastChild
+		if(e.type==="mouseover"){
+			dest.style.color="#4caf50"
+			divdest.style.boxShadow="2px 2px 2px 2px #fff"}
+		else if(e.type==="mouseleave"){
+			dest.style.color="white"
+			divdest.style.boxShadow="0px 0px 0px 0px"}
+	}
+	render()
+	{
+		return(
+			<div className="cardWrapper3">
+			<article className="card" onMouseOver={this.func} onMouseLeave={this.func}>
+			<Link to={this.props.prop.linkto}>	
+			<CardHeader image={this.props.prop.image} link={this.props.prop.linkto}/>
+			<CardBody title={this.props.prop.title}/>
+			</Link>
+			</article>
+			</div>
+		)
+	}
 }
 
 class Card extends React.Component {
-    render() {
-        return (
-        <article className="card">
-            <CardHeader image={this.props.image}/>
-            <CardBody title={this.props.title} text={this.props.text} linkto={this.props.linkto}/>
-        </article>
-        )
-    }
+
+	render() {
+		if (this.props.number==1)
+			return (
+				<Card1 prop={this.props}/>)
+		else if (this.props.number==2)
+			return (
+				<Card2 prop={this.props}/>)
+		else if (this.props.number==3)
+			return (
+				<Card3 prop={this.props}/>)
+	}
+
 }
 export default Card;
+
+
+
+
