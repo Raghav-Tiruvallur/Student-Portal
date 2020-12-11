b74960ca962aeb1cd69ee1063a49269af431187c
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:33:43 +0530

    Update RockOn.js

--- src/Components/RockOn.js
+++ src/Components/RockOn.js
@@ -1,23 +1,32 @@
 import React from 'react';
-
-import {Link} from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
+import EventRegister from "./EventRegister.js"
+import "../EventStyle.css"
 function RockOn(){
-    return(
-        <>
-        <div>
-        <h1 style={{color:"green",textAlign:"center"}}>Rock On</h1><br/>
-        <p style={{color:'blue',textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Get rocking with your band!!
+	return(
+		<div class="clubpage">
+		<div class="topHalf">
+		<h1 style={{color:"#4caf50",textAlign:"center"}}>Rock On</h1><br/>
+		<p style={{WebkitTextFillColor:"white",textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Get rocking with your band!!
         <br/>Let's ge the event started<br/>We hope you're excited too!!<br/>
         We hope to see you there!!<br/>
         Time: 4:00 pm<br/>
         Date: 19/11/2020<br/>
         Amount:Rs.300 per band<br/>
-        #Payment to be made on the day 
-        </p>
-        </div>
-       <Link to='/event/EventRegister'><button style={{marginLeft:'710px',marginTop:'100px',fontSize:20}} className="btn btn-primary btn-sm m-10">Register</button></Link>
-        </>
-    )
+		</p>
+		<button style={{fontSize: "20px",
+			position: "absolute",
+			top: "25vh",
+			right: "10vw"}} className="btn btn-primary btn-sm m-10" onClick={function(){ReactDOM.render(
+				<EventRegister club="Music Club" event="Rock On"/>,document.getElementById("bottomAnchor")
+			)}}>Register</button>
+		</div>
+		<div class="bottomHalf">
+		<div id="bottomAnchor"> </div>
+		</div>
+		</div>
+
+
+	)
 }
-export default RockOn;
+export default RockOn
