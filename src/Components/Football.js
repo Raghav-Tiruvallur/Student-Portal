adb6d69aac1c58830f796d292787140ea13773a3
Raghav-Tiruvallur <60135607+Raghav-Tiruvallur@users.noreply.github.com>
2020-12-11 11:26:43 +0530

    Update Football.js

--- src/Components/Football.js
+++ src/Components/Football.js
@@ -1,23 +1,32 @@
 import React from 'react';
-
-import {Link} from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
+import EventRegister from "./EventRegister.js"
+import "../EventStyle.css"
 function Football(){
-    return(
-        <>
-        <div>
-        <h1 style={{color:"green",textAlign:"center"}}>Football</h1><br/>
-        <p style={{color:'blue',textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Really want to play some football?? Let's get kicking!!
+	return(
+		<div class="clubpage">
+		<div class="topHalf">
+		<h1 style={{color:"#4caf50",textAlign:"center"}}>Football Tournament</h1><br/>
+		<p style={{WebkitTextFillColor:"white",textAlign:"center",fontFamily:"Lucida Console",fontSize:20}}>Really want to play some football?? Let's get kicking!!
         <br/>We are there for you,let's get the match started<br/>You excited? We sure are!!<br/>
         We hope to see you there!!<br/>
         Time: 12:00 noon<br/>
         Date: 20/11/2020<br/>
         Amount:Rs.300 per team<br/>
-        #Payment to be made on the day 
-        </p>
-        </div>
-       <Link to='/event/EventRegister'><button style={{marginLeft:'710px',marginTop:'100px',fontSize:20}} className="btn btn-primary btn-sm m-10">Register</button></Link>
-        </>
-    )
+		</p>
+		<button style={{fontSize: "20px",
+			position: "absolute",
+			top: "25vh",
+			right: "10vw"}} className="btn btn-primary btn-sm m-10" onClick={function(){ReactDOM.render(
+				<EventRegister club="Sports Club" event="Football"/>,document.getElementById("bottomAnchor")
+			)}}>Register</button>
+		</div>
+		<div class="bottomHalf">
+		<div id="bottomAnchor"> </div>
+		</div>
+		</div>
+
+
+	)
 }
 export default Football;
